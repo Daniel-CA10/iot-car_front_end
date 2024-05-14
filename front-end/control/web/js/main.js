5a35e64fbcacbc29bd8a1b58ef1de64dfab9affd
Daniel-CA10 <148724104+Daniel-CA10@users.noreply.github.com>
2024-05-14 12:37:39 -0600

    Add files via upload

---
+++ front-end/control/web/js/main.js
@@ -0,0 +1,52 @@
+let adelante = document.getElementById("adelante");
+let atras = document.getElementById("atras");
+let derecha = document.getElementById("derecha");
+let izquierda = document.getElementById("izquierda");
+let detener = document.getElementById("detener"); 
+let mensaje = document.getElementById("mensaje");
+
+function callAPI(estatus){
+
+
+// Hacer una petición para un usuario con ID especifico
+axios.get('http://localhost/iot-car-control/back-end/apis/setRegistro.php?valorEstatus='+estatus)
+  .then(function (response) {
+    // manejar respuesta exitosa
+    console.log(response);
+    mensaje.innerHTML = "Respuesta: <strong>" + response.data + "</strong>";
+  })
+  .catch(function (error) {
+    // manejar error
+    console.log(error);
+  })
+  .finally(function () {
+    // siempre sera executado
+  });
+
+}
+
+adelante.addEventListener("click",function()
+{
+    callAPI("f");
+})
+
+atras.addEventListener("click",function()
+{
+    callAPI("b");
+})
+
+derecha.addEventListener("click",function()
+{
+  callAPI("r");
+})
+
+izquierda.addEventListener("click",function()
+{
+  callAPI("l");
+})
+
+detener.addEventListener("click",function()
+{
+  callAPI("s");
+})
+
---
+++ front-end/monitor/js/main.js
@@ -0,0 +1,33 @@
+let respuesta = document.getElementById("respuesta");
+
+function callAPIRequest(estatus){
+
+
+// Hacer una petición para un usuario con ID especifico
+axios.get('http://localhost/iot-car-control/back-end/apis/getRegistro.php')
+  .then(function (response) {
+    // manejar respuesta exitosa
+    console.log(response);
+
+    let respuestaServidor = 
+      response.data == "f" ? "Adelante": 
+      response.data == "r" ? "Derecha" :
+      response.data == "l" ? "Izquierda" :
+      response.data == "b" ? "Atras" :
+      "Detener" ;
+
+
+    respuesta.innerHTML = "Respuesta: <strong>" + respuestaServidor + "</strong>";
+  })
+  .catch(function (error) {
+    // manejar error
+    console.log(error);
+  })
+  .finally(function () {
+    // siempre sera executado
+  });
+
+}
+
+
+setInterval(callAPIRequest,2000);
